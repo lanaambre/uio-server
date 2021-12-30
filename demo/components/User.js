30031de30513e2516f3c63bd0da5b5ae9abf436c
Lana-Ambre Demailly <lanaambre.demailly@gmail.com>
2021-12-30 23:01:00 +0100

    Demo: create User component

---
+++ demo/components/User.js
@@ -0,0 +1,44 @@
+module.exports = {
+  name: 'User',
+  path: '/user/:id',
+
+  get({
+    params,
+    $db,
+  }) {
+    return $db.user.getById(params.id);
+  },
+
+  post({
+    body,
+    $db,
+  }) {
+    return $db.user.create(body);
+  },
+
+  delete({
+    params,
+    $db,
+  }) {
+    return $db.user.deleteById(params.id);
+  },
+
+  body: {
+    username: {
+      type: String,
+      minLength: 3,
+      maxLength: 40,
+    },
+    password: {
+      type: String,
+      minLength: 8,
+    },
+    email: {
+      type: String,
+      format: 'email',
+    },
+  },
+  bodyOptions: {
+    removeUnwantedAttributes: true,
+  },
+}
