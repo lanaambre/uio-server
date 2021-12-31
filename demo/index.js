e1677c10582963b894c21a36a7d25570e861c843
Lana-Ambre Demailly <lanaambre.demailly@gmail.com>
2021-12-31 14:32:35 +0100

    Demo: use registerComponents method

--- demo/index.js
+++ demo/index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 const App = require('../lib/App');
 
+const Home = require('./components/Home.js');
+const Article = require('./components/Article.js');
+
 new App({
   host: process.env.HOST,
   port: process.env.PORT,
@@ -9,8 +12,9 @@ new App({
   // .get('/alive')
   // .get('/404', 404)
   // .get('/500', 500)
+  .registerComponents(Home, Article)
   // .registerComponent(Home)
   // .registerComponent(Article)
-  .registerDirectory('components')
+  // .registerDirectory('components')
   .get('/alive')
   .start();
