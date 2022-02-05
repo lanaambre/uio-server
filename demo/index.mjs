9c2b6768223dc9ba3e1ea2d4e270433d44cd4b37
Lana-Ambre Demailly <lanaambre.demailly@gmail.com>
2022-02-05 16:57:59 +0100

    Improve registerDirectory()

--- demo/index.mjs
+++ demo/index.mjs
@@ -2,19 +2,11 @@ import 'dotenv/config.js'; // eslint-disable-line
 
 import App from '../lib/App.mjs';
 
-import Home from './components/Home.mjs';
-import Article from './components/Article.mjs';
-
-new App({
+const app = new App({
   host: process.env.HOST,
   port: process.env.PORT,
-})
-  // .get('/alive')
-  // .get('/404', 404)
-  // .get('/500', 500)
-  .registerComponents(Home, Article)
-  // .registerComponent(Home)
-  // .registerComponent(Article)
-  // .registerDirectory('components')
-  .get('/alive')
-  .start();
+});
+
+await app.registerDirectory('components');
+
+app.start();
