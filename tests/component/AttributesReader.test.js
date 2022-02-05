479c03529e2994601692344afff77180adb2b182
Lana-Ambre Demailly <lanaambre.demailly@gmail.com>
2022-02-05 22:56:05 +0100

    AttributesReader: tests

--- tests/component/AttributesReader.test.js
+++ tests/component/AttributesReader.test.js
@@ -4,7 +4,7 @@ import AttributesReader from '../../lib/component/AttributesReader.mjs';
 
 describe('AttributesReader', function () {
   describe('.process()', function () {
-    it('should only keep apple$red, banana$green, coconut$green, apple', function () {
+    it('should only keep allowed attributes with a maximum of 1 allowed extension on each', function () {
       const attributesReader = new AttributesReader({
         allowedAttributes: ['apple', 'banana', 'coconut'],
         allowedExtensions: ['red', 'green'],
@@ -15,7 +15,6 @@ describe('AttributesReader', function () {
         'banana,coconut$green': 2,
         'apple$red,green': 3,
         lime$green: 4,
-        apple$purple: 5,
         green: 6,
       });
 
@@ -24,6 +23,32 @@ describe('AttributesReader', function () {
         banana$green: 2,
         coconut$green: 2,
       });
+    });
+
+    it('the reader must remove attributes with multiple extensions', function () {
+      const attributesReader = new AttributesReader({
+        allowedAttributes: ['apple', 'banana', 'coconut'],
+        allowedExtensions: ['red', 'green'],
+      });
+
+      const attributes = attributesReader.process({
+        'apple$red,green': 1,
+      });
+
+      assert.equal(attributes, {});
+    });
+
+    it('the reader must remove attributes with a non-allowed extension', function () {
+      const attributesReader = new AttributesReader({
+        allowedAttributes: ['apple', 'banana', 'coconut'],
+        allowedExtensions: ['red', 'green'],
+      });
+
+      const attributes = attributesReader.process({
+        apple$purple: 5,
+      });
+
+      assert.equal(attributes, {});
     });
   });
 });
